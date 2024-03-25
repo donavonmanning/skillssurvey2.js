478de9d3c93e6b531fa1284ccbae2e7b24facc98
donavonmanning1 <156944357+donavonmanning@users.noreply.github.com>
2024-03-25 15:12:13 -0400

    Add files via upload

---
+++ skillssurvey2.js
@@ -0,0 +1,10 @@
+const friend = "BRUTUS";
+const shiftValue = 3;
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const index = friend.indexOf(firstletter.toLowerCase(0));
+const newIndex = index + shiftValue;
+const encryptedFirstLetter = alphabet[newIndex];
+// index letter = 1 + shift vallue = E
+const alphabetLength = alphabet.length;
+const encryptedMessage = "EEUXWXV";
+const teaserMessage = encryptedmessage.slice(0,3);
